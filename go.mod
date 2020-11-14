00191c2c90df4ac8f493b9853d92d80894bb3460
Clement Bolin <clement.bolin@epitech.eu>
2020-11-14 14:18:59 +0100

    add(master): generate new block

---
+++ go.mod
@@ -0,0 +1,9 @@
+module github.com/ClementBolin/blockchain
+
+go 1.15
+
+require (
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/gorilla/mux v1.8.0 // indirect
+	github.com/joho/godotenv v1.3.0 // indirect
+)
--- main.go
+++ main.go
@@ -1,5 +1,56 @@
 package main
 
+import (
+	"crypto/sha256"
+	"encoding/hex"
+	"errors"
+	"time"
+)
+
+// Block is one block of Blockchain
+type Block struct {
+	index 		int
+	timeStamp 	string
+	bpm 		int
+	hash 		string
+	prevHash	string
+}
+
+// Create hash
+func calculHash(block Block) string {
+	record := string(block.index) + block.timeStamp + string(block.bpm) + block.prevHash
+	h := sha256.New()
+	h.Write([]byte(record))
+	return hex.EncodeToString(h.Sum(nil))
+}
+
+// createBlock create new Block structure
+func createBlock(lastBlock Block, BPM int) (Block, error) {
+	var newBlock Block
+	t := time.Now()
+
+	newBlock.index = lastBlock.index + 1
+	newBlock.timeStamp = t.String()
+	newBlock.bpm = BPM
+	newBlock.prevHash = lastBlock.hash
+	newBlock.hash = calculHash(newBlock)
+	return newBlock, nil
+}
+
+// isBlockValid check if newBlock is valid
+func isBlockValid(newBlock, lastBlock Block) error {
+	if lastBlock.index + 1 != newBlock.index {
+		return errors.New("Invalid index")
+	}
+	if lastBlock.hash != newBlock.prevHash {
+		return errors.New("Invalid prev Hash")
+	}
+	if calculHash(newBlock) != newBlock.hash {
+		return errors.New("Invalid Hash")
+	}
+	return nil
+}
+
 func main() {
 	return
 }
